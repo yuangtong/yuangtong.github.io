a54c8fea074c93c6ae10871209d27b2943df5600
yuangtong <yuangtc@gmail.com>
2023-02-01 21:40:47 -0500

    index updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 function alertButton(){
-    alert("Hello World!")
+    alert("Yuyu caca!")
 }
+
+// Adding a "bouncing" animation to the header text
+const header = document.querySelector("header");
+
+header.addEventListener("mouseenter", function() {
+  header.style.animation = "bounce 1s ease";
+});
+
+header.addEventListener("mouseleave", function() {
+  header.style.animation = "";
+});
