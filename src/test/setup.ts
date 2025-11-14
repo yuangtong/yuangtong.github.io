231891e7e655609cd44d10300383a9ca90877da2
Yuang Tong <yuangtc@gmail.com>
2025-11-14 15:37:48 -0500

    test: fix jest-dom setup for Vitest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,8 +1,7 @@
 // Test setup: jest-dom matchers and IntersectionObserver mock
-import { expect, afterEach } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
-expect.extend(matchers);
+import '@testing-library/jest-dom/vitest';
 afterEach(() => cleanup());
 
 type IOCallback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => void;
